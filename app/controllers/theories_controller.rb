84fe3d57eace9d06304f2d46a2a39c6367596132
Gabriel <gabrielcoding97@gmail.com>
2019-11-17 17:58:05 +0000

    links set up in navbar

--- app/controllers/theories_controller.rb
+++ app/controllers/theories_controller.rb
@@ -7,4 +7,15 @@ class TheoriesController < ApplicationController
     @theory = Theory.new
   end
 
+  def create
+    Theory.create(theory_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def theory_params
+    params.require(:theory).permit(:title, :explanation, :writer)
+  end
+
 end
